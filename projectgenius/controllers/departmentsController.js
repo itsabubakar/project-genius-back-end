13ba32b54e52380b2d9c44f4bd45c767854d311f
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-12-15 13:17:34 +0100

    create express sever: login and signUp ready

--- projectgenius/controllers/departmentsController.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file departmentController
- * @author Simpa
- * @copyright Project Genius 2025;
- */
-
-import supabaseClient from "../utils/supabase";
-
-class DepartmentsController {}
--- projectgenius/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import router from './route/index'
-
-const app = express();
-
-app.use(express.json());
-app.use('/', router);
-app.listen(1245);
