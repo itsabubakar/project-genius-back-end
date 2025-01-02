fcd479ed089cf8c4a3ceac15acaf2e27fb947003
A-simpa <mohammedabdulmujeeb16@gmail.com>
2025-01-03 00:41:56 +0100

    (refactor index.js): corrected controllers naming

--- route/index.js
+++ route/index.js
@@ -17,16 +17,16 @@ router.get("/", (req, res) => {
   res.send("Hello Genius!");
 });
 
+router.get("/app/dashboard/:id", AppController.dashboard);
+router.post("/auth/connect", AuthsController.connect);
+router.delete("/auth/disconnect", AuthsController.disconnect);
 router.post("/auth/reset", AuthsController.reset);
 router.post("/auth/reset/finalize", AuthsController.finalizeReset);
-router.delete("/auth/disconnect", AuthsController.disconnect);
-router.get("/app/dashboard/:id", AppController.dashboard);
 router.get("/faculties/:id/depts", FacultiesController.getDeptsByFac);
 router.get("/faculties", FacultiesController.getAll);
 router.get("/teams", TeamsController.getAll);
-router.post("/auth/connect", AuthsController.connect);
 router.post("/users/finalize", UsersController.finalizeSignUp);
 router.post("/users", UsersController.initiateSignUp);
-router.put('/users/:id', UsersController.updateUser);
+router.patch('/users/:id', UsersController.updateUser);
 
 module.exports = router;
