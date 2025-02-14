4afac5b2ac8c739934f096d48f4ad92a7a8fc91a
A-simpa <mohammedabdulmujeeb16@gmail.com>
2025-02-14 18:27:42 +0100

    (server) added cors

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import router from "./route/index";
 
+const cors = require('cors');
 const port = 1245;
 const app = express();
 
+app.use(cors({
+  origin: 'https://project-genius-frontend.vercel.app/', // Allow all origins (you can restrict this later)
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.json());
+
 app.use("/", router);
 app.listen(port, () => {
   console.log(`Server is running and listening on http://localhost:${port}`);
